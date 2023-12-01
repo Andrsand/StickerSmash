58111d76c0081aaaa4de814a2ac9911045094675
andrsand <angorko@list.ru>
2023-12-01 19:43:19 +0300

    Added image selection method

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import ImageViewer from './components/imageViewer';
 import Button from './components/Button';
@@ -7,13 +8,26 @@ import Button from './components/Button';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({ // Использование launchImageLibraryAsync() - метода, который отображает системный пользовательский интерфейс для выбора изображения
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" /> {/* Использование компонента Button */}
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} /> {/* Использование компонента Button */}
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
--- StickerSmash/components/Button.js
+++ StickerSmash/components/Button.js
@@ -1,7 +1,7 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export default function Button({ label, theme }) { // Переопределение основых стилей встоенными 
+export default function Button({ label, theme, onPress }) { // Переопределение основых стилей встоенными 
     if (theme === "primary") {
         return (
             <View
@@ -9,7 +9,7 @@ export default function Button({ label, theme }) { // Переопределен
             >
                 <Pressable
                     style={[styles.button, { backgroundColor: "#fff" }]}
-                    onPress={() => alert('You pressed a button.')}
+                    onPress={onPress}
                 >
                     <FontAwesome
                         name="picture-o"
